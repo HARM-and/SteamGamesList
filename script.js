7ec9320c5fc7a197ca57d82976a3d08d16f390b4
HARMand <84624968+HARM-and@users.noreply.github.com>
2022-09-20 17:22:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,12 @@ const SteamId = [
 [pc = '5', id = '76561198278005950'],
 [pc = '6', id = '76561198278113739'],
 [pc = '7', id = '76561198282739730'],
-[pc = '8', id = ''],
+//[pc = '8', id = '76561198187364982'],
 [pc = '9', id = '76561198281879257'],
 [pc = '10', id = '76561198276954280']
 ]
 
 function sortByName() {
-	$('tbody > tr').sort(function(a, b) {
-	return $('td.game_name', a).text().localeCompare($('td.game_name', b).text());
-	}).appendTo($('tbody'))
-}
-
-function sortPC() {
 	$('tbody > tr').sort(function(a, b) {
 	return $('td.game_name', a).text().localeCompare($('td.game_name', b).text());
 	}).appendTo($('tbody'))
@@ -35,7 +29,6 @@ function removeDuplicate() {
 }
 
 function isDispo(current,pc){
-	console.log(current.attr('pcnb'))
 	var pcnb = current.attr('pcnb')
 	if(pcnb.includes(pc)){
 		return 'dispoPCyes'
@@ -78,7 +71,7 @@ function gamesToTable(gamesList,pc) {
 		}
 		else {
 			try {
-				var row = "<tr class=\"game_cell\" id=\""+game.appid+"\"><td><img class=\"game_img\" src="+gameImg+"></td><td class=\"game_name\">"+game.name+"</td><td>"+timePlayed+"</td><td class=\"pc\" pcnb="+pc+"></td></tr>"
+				var row = "<tr class=\"game_cell\" id=\""+game.appid+"\"><td><img class=\"game_img\" src="+gameImg+"></td><td class=\"game_name\"><a href=\"https://store.steampowered.com/app/"+game.appid+"\" target=\"_blank\" rel=\"noopener noreferrer\">"+game.name+"</a></td><td>"+timePlayed+"</td><td class=\"pc\" pcnb="+pc+"></td></tr>"
 			}
 			catch{
 				var row = "<tr><td>???</td><td></td></tr>"
@@ -103,7 +96,6 @@ request.onload = function() {
 }
 });
 setTimeout(() => {  sortByName(); }, 3000);
-setTimeout(() => {  sortPC(); }, 3000);
 setTimeout(() => {  removeDuplicate(); }, 3500);
 setTimeout(() => {  setPC(); }, 4500);
 
